d1402f4b3114bc71527c90dbb8c0673df9188dca
HyoHwan <gyghks0378@gmail.com>
2023-05-06 14:27:38 +0900

    메인페이지, 마이페이지, 게시글작성 UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Main from './pages/Main'
+import Mypage from './pages/Mypage'
+import Write from './pages/Write'
+import ProfileEdit from './pages/ProfileEdit'
 
 const queryClient = new QueryClient()
 
@@ -10,6 +14,9 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="/profile" element={<ProfileEdit />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
