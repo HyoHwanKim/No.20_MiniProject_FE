782ee5fdb319513e7bb50c8fde4cd17e7f7b412d
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-05-10 16:09:26 +0900

    Chore: react-persist 패키지 추가 (#15)
    * Style: 닫기 버튼 fontawesome으로 변경
    
    * Feat: default 이미지 추가
    
    * Style: reset.css textarea 추가
    
    * Style: 버튼 컴포넌트 color 추가
    
    * Feat: 상세 페이지 UI 구현
    
    * Style: 로그인, 회원가입 공통 스타일 추출
    
    * Feat: 네비바 클릭 시 모달 호출 설정
    
    * Style: import 순서 수정
    
    * Feat: 수정, 삭제 버튼 추가
    
    * Style: 로그인, 회원가입 코드 구성 변경
    
    * Feat: 회원가입 유효성 검증 추가
    
    * Feat: 비밀번호 확인 입력 값에 useRef 설정
    
    * Feat: favicon 추가
    
    * Style: 컴포넌트 스타일드 컴포넌트 작성 방법 수정
    
    * Delete: 사용하지 않는 컴포넌트 삭제
    
    * Feat: 회원가입 기능 추가 후 json-server로 테스트
    
    * Feat: json-server, react-cookie 추가
    
    * Feat: loginUser 리덕스 툴킷 로직 추가
    
    * Feat: 로그인, 회원가입 기능 추가
    
    - 회원가입, 로그인 기능 추가
    - 로그인 시 전역으로 유저 정보 저장되도록 set
    - 아이디 중복확인 임시 처리 (추후 작업 예정)
    
    * Feat: 로그아웃 기능 추가
    
    * Feat: redux-persist 설치

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,7 +1,9 @@
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { setLogoutUser } from '../redux/modules/login'
 import styled from 'styled-components'
 import Button from '../components/Button'
 import Image from '../components/Image'
@@ -12,13 +14,24 @@ const Navbar = () => {
   const [showModal, setShowModal] = useState(false)
   const [menuToggle, setMenuToggle] = useState(false)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  // * 로그인 여부 확인
+  const getLoginInfo = useSelector((state) => state.loginUser)
+  useEffect(() => {
+    console.log(getLoginInfo)
+  }, [getLoginInfo])
 
   const loginModalClickHandler = () => setShowModal(!showModal)
   const closeModalHandler = () => setShowModal(false)
   const toggleClickHandler = () => setMenuToggle(!menuToggle)
 
+  // * 로그아웃
+  const logoutClickHandler = () => {
+    dispatch(setLogoutUser(getLoginInfo.nickname))
+  }
+
   // TODO
-  // 1. 로고 클릭 시 메인화면으로 이동
   // 2. 로그인 했을 경우 & 안했을 경우에 따라 보이는 요소 달라야 함
   // 3. 토글했을 때 어떤 메뉴 보여줄지 & 기능 구현
   // 4. 로그인 하고 나면 이 페이지에서 state 관리해서 뿌려줘야 할듯
@@ -50,7 +63,6 @@ const Navbar = () => {
         <Button
           shape={'circle'}
           color={'transparent'}
-
           onClick={() => {
             navigate('/write')
           }}
@@ -70,13 +82,17 @@ const Navbar = () => {
         {
           menuToggle &&
           <HeaderUl>
-            <HeaderLi onClick={() => {
-              navigate('/mypage')
-            }}>내 비로그</HeaderLi>
-            <HeaderLi onClick={() => {
-              navigate('/profile')
-            }}>프로필 설정</HeaderLi>
-            <HeaderLi>로그아웃</HeaderLi>
+            <HeaderLi onClick={() => navigate('/mypage')}>
+              내 비로그
+            </HeaderLi>
+            <HeaderLi onClick={() => navigate('/profile')}>
+              프로필 설정
+            </HeaderLi>
+            <HeaderLi
+              onClick={logoutClickHandler}
+            >
+              로그아웃
+            </HeaderLi>
           </HeaderUl>
         }
       </HeaderContentRight>
--- src/redux/modules/login.js
+++ src/redux/modules/login.js
@@ -1,6 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = []
+const initialState = {
+  nickname: '',
+  email: '',
+  userImage: '',
+  github: '',
+  description: '',
+}
 
 const loginSlice = createSlice({
   // * 모듈의 이름
@@ -10,12 +16,18 @@ const loginSlice = createSlice({
   // * 모듈의 Reducer 로직
   reducers: {
     setLoginUser: (state, action) => {
-      return [...state, action.payload]
+      return {...state, ...action.payload}
+    },
+    setLogoutUser: (state, action) => {
+      if(state.nickname === action.payload){
+        alert('로그아웃 되었습니다.')
+        return initialState
+      }
     }
   }
 })
 
 // * 컴포넌트에서 사용하기 위해 action creator를 export
-export const { setLoginUser } = loginSlice.actions
+export const { setLoginUser, setLogoutUser } = loginSlice.actions
 // * configStore에 등록하기 위해 reducer를 export default
 export default loginSlice.reducer
