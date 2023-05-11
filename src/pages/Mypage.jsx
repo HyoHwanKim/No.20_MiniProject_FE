b5696d1fd9e83281d74fc91bdb0738fd532ba3f6
HyoHwan <gyghks0378@gmail.com>
2023-05-11 13:39:07 +0900

    마이페이지 수정

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -16,10 +16,16 @@ import {
 } from '../components/styles'
 import Header from './Navbar'
 
+import axios from 'axios'
+import { useQuery } from 'react-query'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+
 
 function Mypage() {
   const navigate = useNavigate()
   const getLoginInfo = useSelector((state) => state.loginUser)
+
 
   // * 내 게시글 조회
   const { data } = useQuery('getMyList', async () => {
@@ -27,30 +33,61 @@ function Mypage() {
     return myList.data
   })
 
-  const boxes = [1, 2, 3, 4, 5]
+  // * 게시글 클릭 시 상세 게시글 조회
+  const openDetail = async (postId) => {
+
+    try {
+      const response = await axios.get(`http://3.34.52.229/api/posts/${postId}`)
+      const post = response.data;
+      navigate(`/detail/${postId}`, { state: { post } })
+
+
+
+    } catch (error) {
+      console.error('API 호출 에러:', error)
+    }
+  }
 
   return (
     <div>
       <Header />
-      <MypageHeader>
-        <MypageHeaderImg src="https://velog.velcdn.com/images/heelieben/post/87bbb462-dbd5-49a5-a9e9-70ed2007cdaf/image.png" alt="" />
-        <MypageHeaderTextContainer>
-          <h2>김효환</h2>
-          <span>개발공부중</span>
-        </MypageHeaderTextContainer>
-      </MypageHeader>
-      <MypageHeaderLine />
+      <MainContainer>
+        <MypageHeader>
+          <MypageHeaderImg src="https://velog.velcdn.com/images/heelieben/post/87bbb462-dbd5-49a5-a9e9-70ed2007cdaf/image.png" alt="" />
+          <MypageHeaderTextContainer>
+            <MypageH2>{getLoginInfo.nickname}</MypageH2>
+            <MypageSpan>{getLoginInfo.description}</MypageSpan>
+          </MypageHeaderTextContainer>
+        </MypageHeader>
+        <MypageHeaderLine />
 
-      <MypageContainer>
-        {boxes.map((box) => (
-          <Box key={box}>
-            <Boximg src="https://velog.velcdn.com/images/heelieben/post/c3dce497-2507-4097-8538-9e3d37cc4933/image.png" />
-            <BoxContent>컨텐트 내용이 들어갑니다.</BoxContent>
-            <BoxFooter>회원이름 / 좋아요버튼</BoxFooter>
-          </Box>
-        ))}
-      </MypageContainer>
+        <MypageContainer>
+          {data && data.posts && data.posts.map((box, index) => (
+            <Box
+              key={index}
+              onClick={() => openDetail(box.postId)}
+            >
+              <Boximg src={`${process.env.PUBLIC_URL}/images/test_thumbnail.png`} />
+              <BoxHeader>{box.title}</BoxHeader>
+              <BoxNickname>@{box.nickname}</BoxNickname>
+              <BoxFooter>
+                {
+                  `
+                  ${box.createdAt.slice(0, 4)}년
+                  ${box.createdAt.slice(5, 7)}월
+                  ${box.createdAt.slice(8, 10)}일
 
+
+                  ${box.createdAt.slice(11, 13)}:
+                  ${box.createdAt.slice(14, 16)}:
+                  ${box.createdAt.slice(17, 19)}
+                  `
+                }
+              </BoxFooter>
+            </Box>
+          ))}
+        </MypageContainer>
+      </MainContainer>
     </div>
   )
 }
