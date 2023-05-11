39e298a920d9424f7f306e19e04011bba7668cc8
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-05-11 10:26:02 +0900

    Feat: 메인 페이지 조회 오류 수정 (#23)
    * Style: 닫기 버튼 fontawesome으로 변경
    
    * Feat: default 이미지 추가
    
    * Style: reset.css textarea 추가
    
    * Style: 버튼 컴포넌트 color 추가
    
    * Feat: 상세 페이지 UI 구현
    
    * Style: 로그인, 회원가입 공통 스타일 추출
    
    * Feat: 네비바 클릭 시 모달 호출 설정
    
    * Style: import 순서 수정
    
    * Feat: 수정, 삭제 버튼 추가
    
    * Style: 로그인, 회원가입 코드 구성 변경
    
    * Feat: 회원가입 유효성 검증 추가
    
    * Feat: 비밀번호 확인 입력 값에 useRef 설정
    
    * Feat: favicon 추가
    
    * Style: 컴포넌트 스타일드 컴포넌트 작성 방법 수정
    
    * Delete: 사용하지 않는 컴포넌트 삭제
    
    * Feat: 회원가입 기능 추가 후 json-server로 테스트
    
    * Feat: json-server, react-cookie 추가
    
    * Feat: loginUser 리덕스 툴킷 로직 추가
    
    * Feat: 로그인, 회원가입 기능 추가
    
    - 회원가입, 로그인 기능 추가
    - 로그인 시 전역으로 유저 정보 저장되도록 set
    - 아이디 중복확인 임시 처리 (추후 작업 예정)
    
    * Feat: 로그아웃 기능 추가
    
    * Feat: redux-persist 설치
    
    * Feat: 로그인 인증 및 인가처리 기능 추가
    
    * Feat: login.js 내용 수정
    
    * Feat: navbar, detail 페이지 데이터에 맞게 수정 진행
    
    * Feat: 로그인 페이지 초기화 버튼 기능 추가
    
    * Docs: 잘못 수정된 파일 복구
    
    * Feat: 회원가입 페이지 초기화 버튼 기능 추가
    
    * Feat: 댓글 조회, 등록 기능 추가
    
    * Feat: 게시글 상세, 댓글 기능 추가 중
    
    * Style: 충돌 수정
    
    * Style: import 수정
    
    * Feat: 새로고침 시 redux 초기화 이슈 처리
    
    * Feat: 메인페이지 조회 오류 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -7,31 +7,21 @@ import {
   BoxFooter
 } from '../components/styles'
 import Header from './Navbar'
-import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { useQuery } from 'react-query'
 import { useNavigate } from 'react-router'
 
 const Main = () => {
-  const [boxes, setBoxes] = useState([])
-
-  const fetchBoxes = async () => {
-    try {
-      const { data } = await axios.get('http://3.34.52.229/api/posts/main')
-      setBoxes(data.posts)
-      console.log("메인페이지 데이터 : ", data)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  useEffect(() => {
-    fetchBoxes()
-  }, [])
-
   const navigate = useNavigate()
 
-  const openDetail = async (postId) => {
+  // * 메인페이지 조회
+  const { data } = useQuery('getMainList', async () => {
+    const mainList = await axios.get(`http://3.34.52.229/api/posts/main`)
+    return mainList.data
+  })
 
+  // * 상세 게시글 조회
+  const openDetail = async (postId) => {
     try {
       const response = await axios.get(`http://3.34.52.229/api/posts/${postId}`)
       const post = response.data;
@@ -46,7 +36,7 @@ const Main = () => {
     <>
       <Header />
       <MainContainer>
-        {boxes.map((box) => (
+        {data && data.posts && data.posts.map((box) => (
           <Box key={box.postId} onClick={() => openDetail(box.postId)}>
             <Boximg src="https://velog.velcdn.com/images/heelieben/post/c3dce497-2507-4097-8538-9e3d37cc4933/image.png" />
             <BoxContent>{box.title}</BoxContent>
