680969a32f5e20fbead6ea448fa1c0caa9a4aa4c
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-05-11 09:39:26 +0900

    Feat: 새로고침 시 RTK 초기화 이슈 처리 (#22)
    * Feat: 새로고침 시 redux 초기화 이슈 처리

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ import './reset.css'
 import { Provider } from 'react-redux'
 import store from './redux/config/configureStore'
 
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+export let persistor = persistStore(store)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
     <App />
+  </PersistGate>
   </Provider>
 );
 
--- src/redux/config/configureStore.js
+++ src/redux/config/configureStore.js
@@ -1,11 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
 import loginUser from '../modules/login'
 
+const reducers = combineReducers({
+  loginUser: loginUser,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['loginUser']
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 const store = configureStore({
-  reducer: {
-    loginUser
-  },
+  reducer: persistedReducer,
 })
 
 export default store
